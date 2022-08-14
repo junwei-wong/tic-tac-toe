2756764c49da97b9a18f6417f63e4d8ec940bc97
Jun <wong.junwei@petronas.com>
2022-08-14 09:35:52 +0800

    Added tie result and rework win logic

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -11,57 +11,67 @@ const GameBoard = () => {
   const [turn, setTurn] = useState(0);
   const [boxes, setBoxes] = useState([...initialState]);
   const [freezeGame, setFreezeGame] = useState(false);
+  const [winner, setWinner] = useState(false);
   const [box1, box2, box3, box4, box5, box6, box7, box8, box9] = boxes;
 
   useEffect(() => {
     if (checkWin(box1, box2, box3)) {
-      showWinnerAlert();
+      setWinner(true);
     }
   }, [box1, box2, box3]);
 
   useEffect(() => {
     if (checkWin(box4, box5, box6)) {
-      showWinnerAlert();
+      setWinner(true);
     }
   }, [box4, box5, box6]);
 
   useEffect(() => {
     if (checkWin(box7, box8, box9)) {
-      showWinnerAlert();
+      setWinner(true);
     }
   }, [box7, box8, box9]);
 
   useEffect(() => {
     if (checkWin(box1, box4, box7)) {
-      showWinnerAlert();
+      setWinner(true);
     }
   }, [box1, box4, box7]);
 
   useEffect(() => {
     if (checkWin(box2, box5, box8)) {
-      showWinnerAlert();
+      setWinner(true);
     }
   }, [box2, box5, box8]);
 
   useEffect(() => {
     if (checkWin(box3, box6, box9)) {
-      showWinnerAlert();
+      setWinner(true);
     }
   }, [box3, box6, box9]);
 
   useEffect(() => {
     if (checkWin(box1, box5, box9)) {
-      showWinnerAlert();
+      setWinner(true);
     }
   }, [box1, box5, box9]);
 
   useEffect(() => {
     if (checkWin(box3, box5, box7)) {
-      showWinnerAlert();
+      setWinner(true);
     }
   }, [box3, box5, box7]);
 
+  useEffect(() => {
+    if (winner) showWinnerAlert();
+  }, [winner]);
+
+  useEffect(() => {
+    if (turn === 9) showTieAlert();
+  }, [turn]);
+
   const resetGame = () => {
+    setWinner(false);
     setBoxes([...initialState]);
     setTurn(0);
     setFreezeGame(false);
@@ -73,11 +83,25 @@ const GameBoard = () => {
     return turnValue % 2 === 0 ? "O" : "X";
   };
 
-  const showWinnerAlert = () => {
-    Swal.fire({
+  const showWinnerAlert = async () => {
+    const previousTurn = turn - 1;
+    await Swal.fire({
       title: "Victory!",
-      text: `${turn % 2 === 0 ? "O" : "X"} wins`,
+      text: `${convertTurnToSymbol(previousTurn)} wins`,
       confirmButtonText: "Cool!",
+      showCancelButton: true,
+      cancelButtonText: "New Game",
+    }).then((result) => {
+      if (result.isDismissed) resetGame();
+      else if (result.isConfirmed) setFreezeGame(true);
+    });
+  };
+
+  const showTieAlert = async () => {
+    await Swal.fire({
+      title: "Tie!",
+      text: "Equally skilled",
+      confirmButtonText: "Aw man!",
       showCancelButton: true,
       cancelButtonText: "New Game",
     }).then((result) => {
